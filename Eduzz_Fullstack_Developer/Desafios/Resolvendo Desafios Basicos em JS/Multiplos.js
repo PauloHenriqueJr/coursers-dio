1997c0bcd8c1391b1dafcc4ef4ad8674ff25d6a1
El Mineiro <84482254+PauloHenriqueJr@users.noreply.github.com>
2021-12-09 16:31:40 -0300

    updates

--- Eduzz_Fullstack_Developer/Desafios/Resolvendo Desafios Basicos em JS/Multiplos.js
+++ Eduzz_Fullstack_Developer/Desafios/Resolvendo Desafios Basicos em JS/Multiplos.js
@@ -1,3 +1,11 @@
+/*Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Nao sao Multiplos", indicando se os valores lidos são múltiplos entre si.
+
+Entrada
+A entrada contém valores inteiros.
+
+Saída
+A saída deve conter uma das mensagens conforme descrito acima.*/
+
 let lines = gets().split("\n");
 
 let line = lines.shift().split(" ");
