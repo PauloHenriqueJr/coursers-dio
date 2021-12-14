d103ad1ce8979a85e1c22289ceb659b3deeaafbe
Paulo Henrique Jr <paulojack2011@gmail.com>
2021-12-14 11:36:44 -0300

    updates

--- Eduzz_Fullstack_Developer/Introdução-ao-React/react-dio/src/components/Card/index.js
+++ Eduzz_Fullstack_Developer/Introdução-ao-React/react-dio/src/components/Card/index.js
@@ -1,38 +1,34 @@
-import { useState } from "react"
+import { useState } from "react";
+import Button from "../Button";
 
 export default function Card() {
-
-    const [valor, setValor] = useState(0)
+    const [valor, setValor] = useState(0);
 
     function Adicionar() {
-        setValor(valor + 1)
+        setValor(valor + 1);
     }
     function Remover() {
-        setValor(valor - 1)
+        setValor(valor - 1);
     }
 
     return (
         <div className="card">
-            <div cclassName="card-header">
+            <div className="card-header">
                 <h1>Meu Primeiro Card</h1>
             </div>
             <div className="card-body">
-                <button
-                    type="button"
-                    className="btn btn-success"
-                    onClick={Adicionar}
-                >
+                <Button 
+                className="btn btn-success" 
+                onClick={Adicionar}>
                     Adicionar
-                </button>
-                <button
-                    type="button"
-                    className="btn btn-danger"
-                    onClick={Remover}
-                >
+                </Button>
+                <Button 
+                className="btn btn-danger" 
+                onClick={Remover}>
                     Remover
-                </button>
+                </Button>
                 <p>{valor}</p>
             </div>
         </div>
-    )
+    );
 }
